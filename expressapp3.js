ae3779a3fedfd14af6d3d5999cefad54abf9feb5
Alex Tanton <atanton@snrt.io>
2017-06-19 13:42:55 -0400

    calendar

--- expressapp3.js
+++ expressapp3.js
@@ -2,7 +2,24 @@ var express = require("express");
 
 var MongoClient = require('mongodb').MongoClient
 var url = 'mongodb://localhost:27017/blogs';
-assert = require('assert');
+var fs = require('fs');
+var https = require('https');
+
+var app = express();
+app.disable('x-powered-by');
+app.set('views', __dirname + '/views');
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.use(require('body-parser').urlencoded({extended: true}))
+
+app.set('port', process.env.PORT || 3000);
+
+var options = {
+	key: fs.readFileSync('domain.key'),
+	cert: fs.readFileSync('cert.crt')
+}
 
 var insertBlog = function(db) {
   var collection = db.collection('documents');
@@ -18,21 +35,17 @@ var insertBlog = function(db) {
 
 MongoClient.connect(url, function(err, db) {
   console.log("Connected successfully to server");
-  	var app = express();
-
-	app.disable('x-powered-by');
-	app.set('views', __dirname + '/views');
-	var handlebars = require('express-handlebars').create({defaultLayout:'main'});
-
-	app.engine('handlebars', handlebars.engine);
-	app.set('view engine', 'handlebars');
-	app.use(require('body-parser').urlencoded({extended: true}))
-
-	app.set('port', process.env.PORT || 3000);
 
 	app.use(express.static(__dirname + '/public'));
 	app.get('/', function(req, res){
-	  res.render('home');
+		db.collection('documents').find().sort({$natural:1}).limit(3).toArray(function(err, docs){
+			if(req.body.contact){
+				var contact = req.body.contact
+			} else {
+				var contact = false;
+			}
+			res.render('home', {top3: docs, contact: contact});
+		})
 	});
 
 	app.get('/blogs', function(req, res){
@@ -45,6 +58,10 @@ MongoClient.connect(url, function(err, db) {
 		insertBlog(db);
 	});
 
+	app.get('/calendar', function(req,res){
+		res.render('calendar', {layout: 'blog.handlebars'})
+	})
+
 	app.get('/blogs/:id', function(req, res){
 		var id = req.params.id;
 		var post = db.collection('documents').find().toArray(function(err, docs){
@@ -52,9 +69,20 @@ MongoClient.connect(url, function(err, db) {
 		})
 	})
 
-	app.listen(app.get('port'), function(){
-	  console.log('Express started on http://localhost:' +
-	    app.get('port') + '; press Ctrl-C to terminate');
-	});
+	app.post('/contact', function(req, res){
+		email = "Email: " + req.body.email + "\n";
+		name = "Name: " + req.body.name + "\n";
+		subject = "Subject: " + req.body.subject + "\n";
+		message = "Message: " + req.body.message + "\n";
+		wr = "---------------------------\n" + name + email + subject + message + "---------------------------"
+		fs.writeFile("test.txt", wr, function(err){
+			console.log(err);
+		})
+		console.log("File written!")
+		res.redirect('/#foot?contact=true');
+	})
+});
 
+https.createServer(options, app).listen(3000, function() {
+    console.log('Server listening on port %d in %s mode', this.address().port, app.settings.env);
 });
